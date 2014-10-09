bf25c972679d94c891d5c813594605af8a0ddf5c
DanielGarcia <pel_daniel@hotmail.com>
2014-10-08 20:49:39 -0500

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PovertyIndicators::Application.config.secret_key_base = 'afef9bf7d5c6a7b88f09cf48ae5282d1436b3f2ebd37d605152a8d4fe6b384357bce562f0b24af93638e586a01bea8d48190b4b772e3eb7040c4d3a0a5aa510c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PovertyIndicators::Application.config.session_store :cookie_store, key: '_poverty-indicators_session'

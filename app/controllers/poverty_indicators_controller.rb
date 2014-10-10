f07780c6aa91e89c829bf969c079f2f7c45e2abf
DanielGarcia <pel_daniel@hotmail.com>
2014-10-09 21:31:29 -0500

    Show form and results of indicator_by_state with the same action

--- app/controllers/poverty_indicators_controller.rb
+++ app/controllers/poverty_indicators_controller.rb
@@ -1,9 +1,8 @@
 class PovertyIndicatorsController < ApplicationController
   def index
-    @graph = IndicatorByState.new
   end
 
   def indicator_by_state
-    @graph = IndicatorByState.new params[:indicator_by_state]
+    @indicator = IndicatorByState.new params[:indicator_by_state] || {}
   end
 end
--- app/models/indicator_by_state.rb
+++ app/models/indicator_by_state.rb
@@ -10,10 +10,13 @@ class IndicatorByState
     end
   end
 
-
   def records
     @records ||= PovertyIndicator.
       select([PovertyIndicator.arel_table[@indicator].send(@formula).as(@indicator), :nom_ent]).
       group(:nom_ent)
+  end
+
+  def empty?
+    !@indicator && !@formula
   end
 end

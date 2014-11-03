84eb6e98bf65e4d02136ab26f8a63c6d5df1113e
DanielGarcia <pel_daniel@hotmail.com>
2014-11-03 08:00:35 -0600

    Fix some styles

--- app/models/indicator_comparison.rb
+++ app/models/indicator_comparison.rb
@@ -18,14 +18,19 @@ class IndicatorComparison
     !@indicator && !@indicator2
   end
 
+  def correlation
+    Statistics.correlation(records.map(&@indicator1.to_sym), records.map(&@indicator2.to_sym))
+  end
 
   private
 
   def compute_correlations
-    records = PovertyIndicator.select([@indicator1, @indicator2, :nom_ent]).to_a
-
     records.group_by { |p| p.nom_ent }.each_with_object({}) do |r, memo|
       memo[r.first.to_sym] = Statistics.correlation(r.last.map(&@indicator1.to_sym), r.last.map(&@indicator2.to_sym))
     end
+  end
+
+  def records
+    @records ||= PovertyIndicator.select([@indicator1, @indicator2, :nom_ent]).to_a
   end
 end
